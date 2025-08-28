5b5a1e1066dfa5cdf866e8cf49b58d232001c667
villabon2008-beep <villabon2008@gmail.com>
2025-08-27 21:09:45 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,17 @@
+<?php
+$identificacion=$_POST [ 'Indetificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST ['Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del empleado es: ".Identificacion;
+echo "lOS NOMBRES Y apellidos del empleado son: ".NomApellidos;
+echo "El cargo del empleado es: ".Salario;
+?>
+
+
+
+
+
+
+
 
